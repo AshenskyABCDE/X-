38c3ce23c4f33c2520af6c818cecd13745f87cff
ashensky <251902756@qq.com>
2024-08-22 23:53:13 +0800

    修改楼中楼回帖

--- tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyAdminController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyAdminController.java
@@ -7,6 +7,7 @@ import com.tianji.learning.domain.query.ReplyPageQuery;
 import com.tianji.learning.domain.vo.ReplyVO;
 import com.tianji.learning.service.IInteractionReplyService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,21 +20,25 @@ import org.springframework.web.bind.annotation.*;
  * @since 2024-08-21
  */
 @RestController
-@RequestMapping("/replies")
+@RequestMapping("/admin/replies")
 @RequiredArgsConstructor
-public class InteractionReplyController {
+public class InteractionReplyAdminController {
 
     private final IInteractionReplyService replyService;
 
 
-    @ApiOperation("新增回答或评论")
-    @PostMapping
-    public void saveReply(@RequestBody ReplyDTO replyDTO) {
-        replyService.saveReply(replyDTO);
-    }
     @ApiOperation("分页查询回答或评论")
     @GetMapping("page")
     public PageDTO<ReplyVO> queryReplyPage(ReplyPageQuery pageQuery){
-        return replyService.queryReplyPage(pageQuery, false);
+        return replyService.queryReplyPage(pageQuery, true);
+    }
+
+    @ApiOperation("隐藏或显示评论")
+    @PutMapping("/{id}/hidden/{hidden}")
+    public void hiddenReply(
+            @PathVariable("id") Long id,
+            @PathVariable("hidden") Boolean hidden
+    ){
+        replyService.hiddenReply(id, hidden);
     }
 }
--- tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionReplyServiceImpl.java
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionReplyServiceImpl.java
@@ -1,10 +1,37 @@
 package com.tianji.learning.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.injector.methods.UpdateById;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.mysql.cj.x.protobuf.MysqlxCrud;
+import com.tianji.api.client.user.UserClient;
+import com.tianji.api.dto.user.UserDTO;
+import com.tianji.common.domain.dto.PageDTO;
+import com.tianji.common.exceptions.DbException;
+import com.tianji.common.utils.BeanUtils;
+import com.tianji.common.utils.CollUtils;
+import com.tianji.common.utils.UserContext;
+import com.tianji.learning.domain.dto.ReplyDTO;
+import com.tianji.learning.domain.po.InteractionQuestion;
 import com.tianji.learning.domain.po.InteractionReply;
+import com.tianji.learning.domain.query.ReplyPageQuery;
+import com.tianji.learning.domain.vo.ReplyVO;
+import com.tianji.learning.enums.QuestionStatus;
+import com.tianji.learning.mapper.InteractionQuestionMapper;
 import com.tianji.learning.mapper.InteractionReplyMapper;
 import com.tianji.learning.service.IInteractionReplyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static com.tianji.common.constants.Constant.DATA_FIELD_NAME_CREATE_TIME;
+import static com.tianji.common.constants.Constant.DATA_FIELD_NAME_LIKED_TIME;
 
 /**
  * <p>
@@ -15,6 +42,120 @@ import org.springframework.stereotype.Service;
  * @since 2024-08-21
  */
 @Service
+@RequiredArgsConstructor
 public class InteractionReplyServiceImpl extends ServiceImpl<InteractionReplyMapper, InteractionReply> implements IInteractionReplyService {
 
+    private final InteractionQuestionServiceImpl questionService;
+
+    private final UserClient userClient;
+
+    @Override
+    public void saveReply(ReplyDTO replyDTO) {
+        InteractionReply reply = BeanUtils.toBean(replyDTO, InteractionReply.class);
+        Long userId = UserContext.getUser();
+        reply.setUserId(userId);
+        save(reply);
+        boolean Is = replyDTO.getAnswerId() == null;
+        if(!Is) {
+            lambdaUpdate()
+                    .setSql("reply_times = reply_times + 1")
+                    .eq(InteractionReply::getId, replyDTO.getAnswerId())
+                    .update();
+        }
+        questionService.lambdaUpdate()
+                .set(Is, InteractionQuestion::getLatestAnswerId, replyDTO.getAnswerId())
+                .setSql(Is,"answer_times = answer_times + 1")
+                .set(replyDTO.getIsStudent(), InteractionQuestion::getStatus, QuestionStatus.UN_CHECK)
+                .eq(InteractionQuestion::getId, reply.getQuestionId())
+                .update();
+    }
+
+    @Override
+    public PageDTO<ReplyVO> queryReplyPage(ReplyPageQuery query, boolean b) {
+        Long questionId = query.getQuestionId();
+        Long answerId = query.getAnswerId();
+        if(questionId == null && answerId == null) {
+            throw new DbException("问题和答案同时为同了");
+        }
+        // 判断是问题下的回答，还是楼中楼
+        boolean Is = query.getAnswerId() == null;
+        Page<InteractionReply> page = lambdaQuery()
+                .eq(Is, InteractionReply::getQuestionId, questionId)
+                .eq(InteractionReply::getAnswerId, Is ? 0L : answerId)
+                .eq(!b, InteractionReply::getHidden, false)
+                .page(query.toMpPage(
+                        new OrderItem(DATA_FIELD_NAME_LIKED_TIME, false),
+                        new OrderItem(DATA_FIELD_NAME_CREATE_TIME, true))
+                );
+        List<InteractionReply> records = page.getRecords();
+        if(CollUtils.isEmpty(records)) {
+            return PageDTO.empty(page);
+        }
+        // 3.数据处理，需要查询：提问者信息、回复目标信息、当前用户是否点赞
+        Set<Long> userIds = new HashSet<>();
+        Set<Long> answerIds = new HashSet<>();
+        Set<Long> targetReplyIds = new HashSet<>();
+        // 3.1.获取提问者id 、回复的目标id、当前回答或评论id（统计点赞信息）
+        for (InteractionReply r : records) {
+            if(!r.getAnonymity() || b) {
+                // 非匿名
+                userIds.add(r.getUserId());
+            }
+            targetReplyIds.add(r.getTargetReplyId());
+            answerIds.add(r.getId());
+        }
+        // 3.2.查询目标回复，如果目标回复不是匿名，则需要查询出目标回复的用户信息
+        targetReplyIds.remove(0L);
+        targetReplyIds.remove(null);
+        if(targetReplyIds.size() > 0) {
+            List<InteractionReply> targetReplies = listByIds(targetReplyIds);
+            Set<Long> targetUserIds = targetReplies.stream()
+                    .filter(Predicate.not(InteractionReply::getAnonymity).or(r -> b))
+                    .map(InteractionReply::getUserId)
+                    .collect(Collectors.toSet());
+            userIds.addAll(targetUserIds);
+        }
+        // 3.3.查询用户
+        Map<Long, UserDTO> userMap = new HashMap<>(userIds.size());
+        if(userIds.size() > 0) {
+            List<UserDTO> users = userClient.queryUserByIds(userIds);
+            userMap = users.stream().collect(Collectors.toMap(UserDTO::getId, u -> u));
+        }
+
+        // 4.处理VO
+        List<ReplyVO> list = new ArrayList<>(records.size());
+        for (InteractionReply r : records) {
+            // 4.1.拷贝基础属性
+            ReplyVO v = BeanUtils.toBean(r, ReplyVO.class);
+            list.add(v);
+            // 4.2.回复人信息
+            if(!r.getAnonymity() || b){
+                UserDTO userDTO = userMap.get(r.getUserId());
+                if (userDTO != null) {
+                    v.setUserIcon(userDTO.getIcon());
+                    v.setUserName(userDTO.getName());
+                    v.setUserType(userDTO.getType());
+                }
+            }
+            // 4.3.如果存在评论的目标，则需要设置目标用户信息
+            if(r.getTargetReplyId() != null){
+                UserDTO targetUser = userMap.get(r.getTargetUserId());
+                if (targetUser != null) {
+                    v.setTargetUserName(targetUser.getName());
+                }
+            }
+        }
+        return new PageDTO<>(page.getTotal(), page.getPages(), list);
+    }
+
+    @Override
+    public void hiddenReply(Long id, Boolean hidden) {
+        UpdateWrapper<InteractionReply> replyUpdateWrapper = new UpdateWrapper<>();
+       replyUpdateWrapper.set("hidden", hidden)
+                .eq("id", id);
+        boolean update = update(replyUpdateWrapper);
+        if(!update) {
+            throw new DbException("状态更改失败了。");
+        }
+    }
 }
